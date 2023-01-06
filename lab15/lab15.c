d5e0f8acecd3f95a4a3a3d5281d066620858a35b
prof <101278408+shpashka@users.noreply.github.com>
2023-01-06 17:56:14 +0300

    Update lab15.c
    Кажется, освобождать только повторяющиеся строки матрицы(двумерный динамический массив через массив указателей) слегка неправильно... Ведь нужно в любом случае освободить память по всем строкам матрицы после работы!

--- lab15/lab15.c
+++ lab15/lab15.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <assert.h>
 //using an array of pointers(array of arrays)
 
 int equal(int a[], int b[], int m, int n){
@@ -13,34 +14,40 @@ int equal(int a[], int b[], int m, int n){
     return flag;
 }
 
+void assert_equal(){
+    int a[] = {1,2,3,4};
+    int b[] = {1,2,3,4};
+    assert(equal(a,b,4,4));
+}
+
 int main(){
     int n;
     int m;
+    int m_const;
     FILE *f;
     f = fopen("arr.txt", "r");
 
     fscanf(f,"%d %d",&m,&n);
-
+    m_const = m;
     int **a = malloc(sizeof(int)*m);
-
     for(int i=0;i<m;i++){
         a[i] = malloc(sizeof(int)*n);
         for(int j=0;j<n;j++){
-            fscanf(f,"%d ",&a[i][j]);
+            fscanf(f,"%d ",&(a[i][j]));
         }
         //printf("%d",*(a+i));
     }
 
-    //freeing the memory from extra matrix lines
-    for(int i=0;i<m;i++){
+
+    for(int i=0;i<m-1;i++){
         for(int j=i+1;j<m;j++){
             if(equal(a[i],a[j],m,n)){
                 int *temp = a[j];
                 for(int k=j;k<m;k++){
                     a[k]=a[k+1];
                 }
-                a[m]=NULL;
-                free(temp);
+                temp=NULL;
+                //free(temp);
                 m-=1;
             }
         }
@@ -54,6 +61,12 @@ int main(){
         printf("\n");
     }
 
+    //freeing all the memory for array ptrs and then the array itself
+    for(int i=0;i<m_const;i++){
+        free((a+i));
+    }
     free(a);
+
+    assert_equal();
     return 0;
 }
