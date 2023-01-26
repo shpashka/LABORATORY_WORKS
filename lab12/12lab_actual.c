78b8c4d452a34e5024d22b5eed28bfcdd6d43f70
prof <101278408+shpashka@users.noreply.github.com>
2023-01-26 14:48:22 +0300

    Add files via upload

---
+++ lab12/12lab_actual.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int get_last(int num){
+    return(num%10);
+}
+
+int get_sum(int *num){
+    if(*num>=10 && get_last(*num)+get_last(*num/10)<10)
+        return(get_last(*num)+get_last(*num/10));
+    else
+        return 0;
+}
+
+void sum(int num){
+    if(num>0){
+        sum(num/10);
+    }
+    if(num>0){
+        printf("%d", get_last(num));
+        if(get_sum(&num))
+            printf("%d", get_sum(&num));
+    }
+
+}
+
+int main(){
+    int number;
+    scanf("%d", &number);
+    sum(number);
+
+}
