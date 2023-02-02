3bd280df089d55b277f01dbe768c49c337df8344
prof <101278408+shpashka@users.noreply.github.com>
2023-02-02 11:55:04 +0300

    Add files via upload
    The program has been altered according to requirements and now its logic is based on the finite automaton

--- lab11/strings.c
+++ lab11/strings.c
@@ -2,112 +2,113 @@
 #include <assert.h>
 #include <windows.h>
 
-int isdigit(char c){
-    return((c>='0') && (c<='9'));
-}
 
-int decimal_found(char buf[]){
-    //logical formula telling whether there is a "0110" situation when using isdigit() to buf[]
-    return( (!isdigit(buf[0])&&!isdigit(buf[3])) && (isdigit(buf[1])&&isdigit(buf[2])) );
-}
+typedef enum{
+    s0,
+    s1,
+    s2,
+    s3,
+    s4
+}State;
 
-void write_num(char prev, char current){
-    //printf("%d",(prev<='9')&&(prev>='1')&&(current>='0')&&(current<=9));
-    if((prev<='9')&&(prev>='1')&&(current>='0')&&(current<='9')){
-        //printf("  %c%c  ",prev,current);
-        if(prev=='1'){
-            switch(current){
-            /*case '1':
-                printf("Одиннадцать\t");
-                break;
-            case '2':
-                printf("Двенадцать\t");
-                break;
-            case '3':
-                printf("Тринадцать\t");
-                break;
-            case '4':
-                printf("Четырнадцать\t");
-                break;
-            case '5':
-                printf("Пятнадцать\t");
-                break;
-            case '6':
-                printf("Шестнадцать\t");
-                break;*/
-            case '7':
-                printf("Семнадцать\t");
-                break;
-            case '8':
-                printf("Восемнадцать\t");
-                break;
-            case '9':
-                printf("Девятнадцать\t");
-                break;
-            }
-        }
 
-        else{
-            switch(prev){
-            case '2':
-                printf("Двадцать ");
-                break;
-            case '3':
-                printf("Тридцать ");
-                break;
-            case '4':
-                printf("Сорок ");
-                break;
-            case '5':
-                printf("Пятьдесят ");
-                break;
-            case '6':
-                printf("Шестьдесят ");
-                break;
-            case '7':
-                printf("Семьдесят ");
-                break;
-            case '8':
-                printf("Восемьдесят ");
-                break;
-            case '9':
-                printf("Девяносто ");
-                break;
-            }
-            switch(current){
-            case '0':
-                printf("\t");
-                break;
-            case '1':
-                printf("один\t");
-                break;
-            case '2':
-                printf("два\t");
-                break;
-            case '3':
-                printf("три\t");
-                break;
-            case '4':
-                printf("четыре\t");
-                break;
-            case '5':
-                printf("пять\t");
-                break;
-            case '6':
-                printf("шесть\t");
-                break;
-            case '7':
-                printf("семь\t");
-                break;
-            case '8':
-                printf("восемь\t");
-                break;
-            case '9':
-                printf("девять\t");
-                break;
-            }
+
+
+void write_num(int prev, int current){
+    assert((prev<=9)&&(prev>=1)&&(current>=0)&&(current<=9));
+    if(prev==1){
+        switch(current){
+        /*case '1':
+            printf("Одиннадцать\t");
+            break;
+        case '2':
+            printf("Двенадцать\t");
+            break;
+        case '3':
+            printf("Тринадцать\t");
+            break;
+        case '4':
+            printf("Четырнадцать\t");
+            break;
+        case '5':
+            printf("Пятнадцать\t");
+            break;
+        case '6':
+            printf("Шестнадцать\t");
+            beak;*/
+        case 7:
+            printf("Семнадцать\t");
+            break;
+        case 8:
+            printf("Восемнадцать\t");
+            break;
+        case 9:
+            printf("Девятнадцать\t");
+            break;
         }
     }
+
+    else{
+        switch(prev){
+        case 2:
+            printf("Двадцать ");
+            break;
+        case 3:
+            printf("Тридцать ");
+            break;
+        case 4:
+            printf("Сорок ");
+            break;
+        case 5:
+            printf("Пятьдесят ");
+            break;
+        case 6:
+            printf("Шестьдесят ");
+            break;
+        case 7:
+            printf("Семьдесят ");
+            break;
+        case 8:
+            printf("Восемьдесят ");
+            break;
+        case 9:
+            printf("Девяносто ");
+            break;
+        }
+        switch(current){
+        case 0:
+            printf("\t");
+            break;
+        case 1:
+            printf("один\t");
+            break;
+        case 2:
+            printf("два\t");
+            break;
+        case 3:
+            printf("три\t");
+            break;
+        case 4:
+            printf("четыре\t");
+            break;
+        case 5:
+            printf("пять\t");
+            break;
+        case 6:
+            printf("шесть\t");
+            break;
+        case 7:
+            printf("семь\t");
+            break;
+        case 8:
+            printf("восемь\t");
+            break;
+        case 9:
+            printf("девять\t");
+            break;
+        }
+    }
+
 }
 
 
@@ -115,18 +116,62 @@ void write_num(char prev, char current){
 int main(){
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+    char symbol;
+    int digit1;
+    int digit2;
 
-    char buffer[4] = {' ',' ',' ',' '}; //we are interested in this situation [non-digit,digit,digit,nondigit]
-
-    while((buffer[3] = getchar())!= EOF){
-        //we need not the copy but the array itself. Fortunately, it is default in C
-        if(decimal_found(buffer)){
-            write_num(buffer[1],buffer[2]);
+    State state = s0;
+    while((symbol = getchar())!= EOF){
+        int digit = symbol-'0'; // it is an integer made from char if it is a digit(else it is a meaningless number)
+        switch(state){
+        case(s0): // s0,s1,s2,s3,s4 define a DefiniteFiniteAutomaton - see the automaton in repository
+            if(digit==1){
+                state = s1;
+                digit1 = digit;
+            }
+            else if(digit>=2 && digit<=9){
+                state = s2;
+                digit1 = digit;
+            }
+            else
+                state = s0;
+            break;
+        case(s1):
+            if(digit>=7 && digit<=9){
+                state = s3;
+                digit2 = digit;
+            }
+            else if(digit>=0 && digit<=6)
+                state = s4;
+            else
+                state = s0;
+            break;
+        case(s2):
+            if(digit>=0 && digit<=9){
+                state = s3;
+                digit2 = digit;
+            }
+            else
+                state = s0;
+            break;
+        case(s4):
+            if(digit>=0 && digit<=9)
+                state = s4;
+            else
+                state = s0;
+            break;
+        case(s3): // final state which means that the number has been found
+            if(digit>=0 && digit<=9)
+                state = s4;
+            else
+                write_num(digit1,digit2);
+                state = s0;
+            break;
         }
-        buffer[0]=buffer[1];
-        buffer[1]=buffer[2];
-        buffer[2]=buffer[3];
-    }
 
+
+
+
+    }
     return 0;
 }
